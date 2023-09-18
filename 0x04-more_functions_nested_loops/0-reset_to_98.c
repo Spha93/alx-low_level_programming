d89c8fd7a3a95c32b8daf8be77c6507340377341
Spha93 <ndumo.st@gmail.com>
2023-09-18 08:01:56 -0700

    0-reset_to_98.c

---
+++ 0x04-more_functions_nested_loops/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset _to_98 - a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @n: input
+ * Return: n
+ */
+void reset_to_989(int *n)
+{
+	*n = 98;
+}
